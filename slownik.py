5740dd9f5055eb42306653cfd712a45652e1f956
xwoytek <115814508+xwoytek@users.noreply.github.com>
2022-11-23 17:38:04 +0100

    Add files via upload

---
+++ slownik.py
@@ -0,0 +1,17 @@
+japwn = {
+        "imie":"Kemal",
+        "nazwisko":"Schultz",
+        "praca":"komornik sądowy",
+        "edukacja" : "najniższa",
+        "data urodzin" : '12 345 678 901 234 567 890 p.n.e.',
+        "hobby": 'podbijanie ludów germańskich',
+        'poglądy': 'anarchista',
+        'uzależnienia': 'likwidowanie władców',
+        'ilość rodzeństwa': '745',
+        'wzrost': '21 burgerów drwala',
+        'objętość': '4 678 912 hektolitrów',
+        'waga': '478 012 czołgów M1A2 Abrams'
+}
+
+for k,v in japwn.items():
+    print(f'co: {k}, ile: {v} \n')
